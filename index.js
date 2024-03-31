94eee44d61fa33407e8118d81cd76244fc030501
Danielarceeno <daniel7ar@gmail.com>
2024-03-31 13:17:33 -0300

    feat: creat News Schema

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 import express from "express";
 import connectDatabase from "./scr/database/db.js";
-import userRoute from "./scr/routes/user.route.js";
 import dotenv from "dotenv";
+
+import userRoute from "./scr/routes/user.route.js";
+import authRoute from "./scr/routes/auth.route.js";
 
 dotenv.config();
 
-const app = express();
 const port = process.env.PORT || 3000;
+const app = express();
 
 connectDatabase();
 app.use(express.json());
 app.use("/user", userRoute);
-
+app.use("/auth", authRoute);
 app.listen(port, () => console.log(`Server is running in port ${port}`) );

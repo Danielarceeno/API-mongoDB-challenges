45af5bcd2bb4e35917a6f90f5550c7c5dbecf48c
Danielarceeno <daniel7ar@gmail.com>
2024-03-31 18:23:04 -0300

    express title

--- scr/controllers/news.controller.js
+++ scr/controllers/news.controller.js
@@ -4,6 +4,7 @@ import {
   countNews,
   topNewsService,
   findByIdService,
+  searchByTitleService,
 } from "../services/news.service.js";
 
 export const create = async (req, res) => {
@@ -125,6 +126,35 @@ export const findById = async (req, res) => {
         username: news.user ? news.user.username : null,
         userAvatar: news.user ? news.user.avatar : null,
       },
+    });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
+
+export const searchByTitle = async (req, res) => {
+  try {
+    const { title } = req.query;
+    const news = await searchByTitleService(title);
+
+    if (news.length === 0) {
+      return res
+        .status(400)
+        .send({ message: "There are no news with this title" });
+    }
+
+    return res.send({
+      results: news.map((item) => ({
+        id: item._id,
+        title: item.title,
+        text: item.text,
+        banner: item.banner,
+        likes: item.likes,
+        comments: item.comments,
+        name: item.user ? item.user.name : null,
+      username: item.user ? item.user.username : null,
+      userAvatar: item.user ? item.user.avatar : null,
+      })),
     });
   } catch (err) {
     res.status(500).send({ message: err.message });
